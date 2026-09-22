6f783046e748ca20263a7f86e9b8232c8e0324df
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-22 17:01:39 +0300

    Expose public Python API

---
+++ src/dev_environment_checker/__init__.py
@@ -0,0 +1,4 @@
+"""Local development environment auditing."""
+from .core import CheckResult, audit, check_tool, load_policy
+__all__ = ["CheckResult", "audit", "check_tool", "load_policy"]
+__version__ = "1.0.0"
